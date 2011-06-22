23d618da3d0d8e7ebe16788cc7e345386a0ae0b5
Carlos Correa <razor950@gmail.com>
2011-06-22 14:12:21 -0400

    added ext4 checking/conversion

--- updater.sh
+++ updater.sh
@@ -5,3 +5,35 @@
 
 set -x
 export PATH=/:/sbin:/system/xbin:/system/bin:/tmp:$PATH
+
+test_mount()
+{
+	if ! /tmp/busybox mount -t ext4 /dev/block/$2 /$1
+             umount_format $1 $2
+	else 
+           echo "Partition is already EXT4."
+        fi
+}
+
+umount_format()
+{
+        echo "Unmounting $1 on $2"
+        /tmp/busybox umount /$1
+        if [ $(/tmp/busybox mount | grep "/dev/block/$2 on /$1 type" | wc -l) -eq "0" ]; then
+                echo "Formatting $1 on $2 to EXT4"
+                /tmp/make_ext4fs -b 4096 -g 32768 -i 8192 -I 256 -a /$1 /dev/block/$2
+                echo "Successfully converted $1 on $2"
+        else
+                echo "Cannot unmount $1 on $2"
+                exit $;
+        fi
+}
+
+
+# check each partition
+test_mount system stl9
+test_mount cache stl11 
+test_mount dbdata stl10
+test_mount data mmcblk0p2
+
+exit 0;
