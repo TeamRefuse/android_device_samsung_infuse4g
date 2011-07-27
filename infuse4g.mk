2191011118104e4fd7306f4582e6bbb05a13c4ad
M. Adam Kendall <linuxbozo@gmail.com>
2011-07-27 00:19:33 -0400

    argh. ok, let's try this again. wpa_supplicant removed

--- infuse4g.mk
+++ infuse4g.mk
@@ -77,10 +77,6 @@ PRODUCT_COPY_FILES += \
 	device/samsung/aries-common/sec_mm/sec_omx/sec_omx_core/secomxregistry:system/etc/secomxregistry \
 	device/samsung/infuse4g/media_profiles.xml:system/etc/media_profiles.xml
 
-PRODUCT_COPY_FILES += \
-	device/samsung/infuse4g/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf
-
-
 # These are the OpenMAX IL modules
  PRODUCT_PACKAGES += \
  	libSEC_OMX_Core.aries \
